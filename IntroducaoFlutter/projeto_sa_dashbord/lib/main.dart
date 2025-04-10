72e9a12dd513de55d40ef18e564507a953c4ed87
Nickelodeon <anickmurilomeyretuti@gmail.com>
2025-04-10 08:31:58 -0300

    Update main.dart

--- IntroducaoFlutter/projeto_sa_dashbord/lib/main.dart
+++ IntroducaoFlutter/projeto_sa_dashbord/lib/main.dart
@@ -1,5 +1,5 @@
+// imports do widget MaterialApp e das outras telas
 import 'package:flutter/material.dart';
-
 import 'view/tela_infografico_view.dart';
 import 'view/tela_inicial_view.dart';
 import 'view/tela_tarefas_view.dart';
@@ -7,12 +7,12 @@ import 'view/tela_tarefas_view.dart';
 void main() {
   runApp(
     MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: "/",
+      debugShowCheckedModeBanner: false, // tirar a faixa de debug
+      initialRoute: "/", // definir rota inicial
       routes: {
-        "/": (context) => TelaInicial(),
-        "/tarefas": (context) => TelaTarefas(),
-        "/infografico": (context) => TelaInfografico(),
+        "/": (context) => TelaInicial(), // atribui a rota "/" para a tela inicial
+        "/tarefas": (context) => TelaTarefas(), // atribui a rota "/tarefas" para a tela de tarefas
+        "/infografico": (context) => TelaInfografico(), // atribui a rota "/infografico" para a tela do dashboard
       },
     ),
   );
