2150ec4c8885c9796ffceaf5a720eab17dab5fb5
Miickkk <anickmurilomeyretuti@gmail.com>
2025-09-17 16:20:08 -0300

    17.09.25

--- ApisWebServices/JsonConvert.dart
+++ ApisWebServices/JsonConvert.dart
@@ -1,28 +1,26 @@
 //Teste de Conversão Json <-> Dart
-import 'dart:convert'; //nativa -> não precisa instalar para o pubspec
+import 'dart:convert'; //nativa -> não precisa baixar para o pubspec
 
 void main(){
   //tenho um texto em formato de JSON
-  
   String UsuarioJson = '''{
-                             "id": "1ab2",
-                             "user": "usuario1",
-                             "nome": "Maria L",
-                             "idade": 19,
-                             "cadastrado": true
-                      }'''; //tres aspas voce consegue escrever quebrando linha
-
-  // para manipular o texto
-  //converter (decode) JSON -> MAP                      
+                            "id": "1ab2",
+                            "user": "usuario1",
+                            "nome": "Pedro",
+                            "idade": 25,
+                            "cadastrado": true
+                      }''';
+  // para manipular o texto 
+  // converter(decode) JSON em MAP
   Map<String,dynamic> usuario = json.decode(UsuarioJson);
-
-  //manipulando informações do JSON -> MAP
+  // manipulando informações do JSON -> Map
   print(usuario["idade"]);
-  usuario["idade"] = 21;
-
-  //converter (encode) de MAP -> JSON 
+  usuario["idade"] = 26;
+  // converter(encode) de Map -> Json
   UsuarioJson = json.encode(usuario);
-
   //tenho novamente um JSON em formato de Texto
   print(UsuarioJson);
+
+  
 }
+
